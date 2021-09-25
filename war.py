3a055eeca14f7845d37b3c94f55a301e39bdfbd0
hayuh <hyuh@andrew.cmu.edu>
2021-09-25 19:36:14 -0400

    Added checks for winners

--- war.py
+++ war.py
@@ -15,45 +15,54 @@ print(half)
 player_1 = deque(cards[0:half])
 player_2 = deque(cards[half:])
 
-
-print("1", player_1)
-print("2", player_2)
-#while(len(player1) != 0 and len(player2) != 0):
-player_1_card, player_2_card = player_1.pop(), player_2.pop()
-print("1", player_1)
-print("2", player_2)
-if(deck[player_1_card] > deck[player_2_card]):
-        player_1.extendleft([player_1_card, player_2_card])
-        print("1", player_1)
-        print("2", player_2)
-elif(deck[player_1_card] < deck[player_2_card]):
-        player_2.extendleft([player_1_card, player_2_card])
-        print("1", player_1)
-        print("2", player_2)
-else:
-        winning_pot = [player_1_card, player_2_card]
-        war_won = False
-        while(war_won == False and (len(player_1) > 1 and len(player_2) > 1)):
-                player_1_war_card_down, player_1_war_card_up = player_1.pop(), player_1.pop()
-                player_2_war_card_down, player_2_war_card_up = player_2.pop(), player_2.pop()
-                winning_pot.extend([player_1_war_card_down, player_1_war_card_up, player_2_war_card_down, player_2_war_card_up])
-                if(deck[player_1_war_card_up] > deck[player_2_war_card_up]):
-                        player_1.extendleft(winning_pot)
-                        war_won = True
-                        print("1", player_1)
-                        print("2", player_2)
-                elif(deck[player_1_war_card_up] < deck[player_2_war_card_up]):
-                        player_2.extendleft(winning_pot)
-                        war_won = True
-                        print("1", player_1)
-                        print("2", player_2)
-                else:
-                        continue
-
-a = deque([1,2,3,4])
-print(a)
-a_down, a_up = a.pop(), a.pop()
-print(a_down, a_up, a)
+num_war_deals = 2
+print("1 start", player_1)
+print("2 start", player_2)
+while(1):
+        if(len(player_1) == 0):
+                print("Player 2 wins")
+                break
+        if(len(player_2) == 0):
+                print("Player 1 wins")
+                break
+        player_1_card, player_2_card = player_1.pop(), player_2.pop()
+        if(deck[player_1_card] > deck[player_2_card]):
+                player_1.extendleft([player_1_card, player_2_card])
+                print("1 battle", player_1)
+                print("2 battle", player_2)
+        elif(deck[player_1_card] < deck[player_2_card]):
+                player_2.extendleft([player_1_card, player_2_card])
+                print("1 battle", player_1)
+                print("2 battle", player_2)
+        else:
+                winning_pot = [player_1_card, player_2_card]
+                war_won = False
+                while(war_won == False):
+                        if(len(player_1) < num_war_deals and len(player_2) < num_war_deals):
+                                print("Draw")
+                                break
+                        if(len(player_1) < num_war_deals):
+                                print("Player 2 wins")
+                                break
+                        if(len(player_2) < num_war_deals):
+                                print("Player 1 wins")
+                                break
+                        for i in range(0, num_war_deals-1):
+                                winning_pot.extend([player_1.pop(), player_2.pop()])
+                        player_1_war_card, player_2_war_card = player_1.pop(), player_2.pop()
+                        winning_pot.extend([player_1_war_card, player_2_war_card])
+                        if(deck[player_1_war_card] > deck[player_2_war_card]):
+                                player_1.extendleft(winning_pot)
+                                war_won = True
+                                print("1 war", player_1)
+                                print("2 war", player_2)
+                        elif(deck[player_1_war_card] < deck[player_2_war_card]):
+                                player_2.extendleft(winning_pot)
+                                war_won = True
+                                print("1 war", player_1)
+                                print("2 war", player_2)
+                        else:
+                                continue
 
 
 
